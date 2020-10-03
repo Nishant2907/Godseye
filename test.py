e95b88ef4643b75cbafb1f061f248c6421d113c9
Nishant2907 <nishdrummer@gmail.com>
2020-10-04 01:35:20 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,38 @@
+from cv2 import cv2
+import face_recognition
+
+# FOR LOADING IMAGE FILE FROM "ASSETS" FOLDER
+imgJohnny = face_recognition.load_image_file('assets/Johnny-Depp.jpg')
+
+# ABOVE IMAGE WAS IN BGR COLOR FORMAT
+# CONVERTING IT TO RGB COLOR FORMAT
+imgJohnny= cv2.cvtColor(imgJohnny, cv2.COLOR_BGR2RGB)
+
+# FINDING THE FACE IN THE IMAGE
+johnnyLocation = face_recognition.face_locations(imgJohnny)[0]
+
+# GETTING 128 MEASUREMENTS OF THE FACE
+johnnyEncode = face_recognition.face_encodings(imgJohnny)[0]
+
+# REPEATING THE SAME STEP FOR A TEST IMAGE
+imgJohnnytest = face_recognition.load_image_file('assets/johnydepp-test.jpg')
+imgJohnnytest = cv2.cvtColor(imgJohnnytest, cv2.COLOR_BGR2RGB)
+johnnytestLocation = face_recognition.face_locations(imgJohnnytest)[0]
+johnnytestEncode = face_recognition.face_encodings(imgJohnnytest)[0]
+
+result = face_recognition.compare_faces([johnnyEncode],johnnytestEncode)
+print(result)
+
+# IF FACES MATCH --> BOTH IMAGES WILL HAVE GREEN RECTANGLE
+# ELSE TEST IMAGE WILL HAVE RED RECTANGLE
+if(result[0]):
+    cv2.rectangle(imgJohnny, (johnnyLocation[3], johnnyLocation[0]), (johnnyLocation[1], johnnyLocation[2]), (0, 255, 0), 2)
+    cv2.rectangle(imgJohnnytest, (johnnytestLocation[3], johnnytestLocation[0]), (johnnytestLocation[1], johnnytestLocation[2]), (0, 255, 0), 2)
+else:
+    cv2.rectangle(imgJohnny, (johnnyLocation[3], johnnyLocation[0]), (johnnyLocation[1], johnnyLocation[2]), (0, 255, 0), 2)
+    cv2.rectangle(imgJohnnytest, (johnnytestLocation[3], johnnytestLocation[0]), (johnnytestLocation[1], johnnytestLocation[2]), (0, 0, 255), 2)
+
+# SHOWING THE FINAL IMAGE WITH GREEN RECTANGLE
+cv2.imshow('Johnny Depp', imgJohnny)
+cv2.imshow('Johnny Depp Test', imgJohnnytest)
+cv2.waitKey(0)
