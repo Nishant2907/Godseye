8e7ab5008b3b60f9c67a4c61884010bb7207b2dd
Hardikkr <hardik.kumar18feb@gmail.com>
2020-10-13 02:58:05 +0530

    worked on list generator

--- videoCode[test2].py
+++ videoCode[test2].py
@@ -3,6 +3,21 @@
 from cv2 import cv2
 import face_recognition
 import os
+import math
+import numpy
+
+vid = cv2.VideoCapture(0)
+
+# FOR CHECKING THE ACCURACY
+def face_distance_to_conf(face_distance, face_match_threshold=0.4718):
+    if face_distance > face_match_threshold:
+        range = (1.0 - face_match_threshold)
+        linear_val = (1.0 - face_distance) / (range * 2.0)
+        return linear_val
+    else:
+        range = face_match_threshold
+        linear_val = 1.0 - (face_distance / (range * 2.0))
+        return linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))
 
 # NAME AND PATH OF EACH PERSON
 allPath = os.listdir("testAssets")
@@ -11,27 +26,64 @@ allEncode = []
 for i in range(len(allPath)):
     allName.append(allPath[i].split(".")[0])
     img = face_recognition.load_image_file("testAssets/" + allPath[i])
-    allEncode.append(face_recognition.face_encodings(img))
+#    allEncode.append(face_recognition.face_encodings(img))
 
-print(allName)
-vid = cv2.VideoCapture()
+for i in range(len(allPath)):
+    for encode in allName:
+        temp = encode
+        img = face_recognition.load_image_file("testAssets/" + allPath[i])
+        temp = face_recognition.face_encodings(img)
+        allEncode.append(temp)
 
+#nishant = face_recognition.load_image_file("assets/nishant_cropped.jpg")
+#nishant = face_recognition.face_encodings(nishant)[0]
+
+#hardik = face_recognition.load_image_file("assets/hardik.jpg")
+#hardik = face_recognition.face_encodings(hardik)[0]
+
+#allName = ["nishant", "hardik"]
+#allEncode = [nishant, hardik]
+
+X = True
 while True:
-    frame = vid.read()
+    ret, frame = vid.read()
 
-    #frame = cv2.resize(frame,(0,0),None,0.25,0.25)
+    resizeFrame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    checkFrame = resizeFrame[:, :, ::-1]
 
-    facelocation = face_recognition.face_locations(frameRGB)
-
-    faceencode = face_recognition.face_encodings(frameRGB)
-
-    for i in range(len(allPath)):
-        result = face_recognition.compare_faces([allEncode[i]], frameRGB)
-        if(result[0]):
-            cv2.rectangle(frameRGB, (100, 100), (100, 100), (0, 255, 0), 2)
-        else:
-            cv2.rectangle(frameRGB, (100, 100), (100, 100), (0, 0, 255), 2)
     
+    if X:
+        faceLocation = face_recognition.face_locations(checkFrame)
+
+        faceEncode = face_recognition.face_encodings(checkFrame, faceLocation)
+
+        #for i in range(len(allPath)):
+        #    result = face_recognition.compare_faces([allEncode[i]], frame)
+        #    if(result[0]):
+        #        cv2.rectangle(frame, (100, 100), (100, 100), (0, 255, 0), 2)
+        #    else:
+        #        cv2.rectangle(frame, (100, 100), (100, 100), (0, 0, 255), 2)
+
+        for i in faceEncode:
+            match = face_recognition.compare_faces(allEncode, i)
+
+            name = "Unknown"
+
+            faceDistance = face_recognition.face_distance(allEncode, i)
+            bestMatchIndex = numpy.argmin(faceDistance)
+
+            if match[bestMatchIndex]:
+                name = allName[bestMatchIndex]
+                print(allName[bestMatchIndex])
+
+    x = not X
+
+    #cv2.rectangle(frame, (faceLocation[3], faceLocation[2]), (faceLocation[1], faceLocation[0]), (0, 255, 0), 2)
+    cv2.imshow('Recording video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
 
